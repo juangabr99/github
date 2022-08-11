a5dc4104fb0dd101bd4654f3d6da76abe37e2bcc
juangabr99 <111038421+juangabr99@users.noreply.github.com>
2022-08-10 23:23:45 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+$(function(){
+    $("html").css({"font-size": "60%"});
+    $("body").css({"font-size": "16px"});
+    $(".header").css({"padding": "1rem", "background-color": "var(--negro)"});
+    $(".titulo").css({"text-align": "center", "font-family": "'Raleway', sans-serif", "color":"var(--rojo)", "text-transform": "uppercase"});
+    $(".sectionEspecificos").css({"display": "flex", "flex-direction": "column", "align-items":"center", "text-transform":"uppercase"});
+});
